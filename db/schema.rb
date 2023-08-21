60865624d6f1269ef5e142a141a72cb4c847c011
clara wilner <clara.luna.wilner@gmail.com>
2023-08-21 16:10:49 +0200

    reservation model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_133653) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_141005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_133653) do
     t.index ["user_id"], name: "index_grannies_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "category_booked"
+    t.bigint "user_id", null: false
+    t.bigint "granny_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["granny_id"], name: "index_reservations_on_granny_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_133653) do
 
   add_foreign_key "categories", "grannies"
   add_foreign_key "grannies", "users"
+  add_foreign_key "reservations", "grannies"
+  add_foreign_key "reservations", "users"
 end
