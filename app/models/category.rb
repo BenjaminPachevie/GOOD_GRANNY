498c641cbeafd505f2a0ad0fd76063622d1eabfe
clara wilner <clara.luna.wilner@gmail.com>
2023-08-21 17:50:38 +0200

    validations added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  CATEGORIES = ["cooking", "sewing", "gardening", "life advices"]
   belongs_to :granny
+  validates :name, presence: true, inclusion: { in: CATEGORIES }
+
 end
--- app/models/granny.rb
+++ app/models/granny.rb
@@ -2,4 +2,8 @@ class Granny < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_many :categories
+  has_one_attached :photo
+  validates :name, presence: true, uniqueness: true
+  validates :presentation, length: { minimum: 14 }
+  validates :location, :price, presence: true
 end
