3ef829324144fea39c7900df55765b50e125c896
Benjamin Pachevie <b.pachevie@gmail.com>
2023-08-22 16:04:58 +0200

    updated granny model with array locations

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -1,6 +1,6 @@
 class Granny < ApplicationRecord
   belongs_to :user
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :categories
   has_many_attached :photos
   validates :name, presence: true, uniqueness: true
