6f206a75752d5d8c6eb036972bfba365d0e41720
clara wilner <clara.luna.wilner@gmail.com>
2023-08-22 11:41:31 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :grannies do
+    resources :reservations, only: :create
+  end
+  resources :reservations, only: :destroy
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
