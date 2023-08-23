aeb9a952fcf01fb6d69e1600d8a9a5b983b950b4
Benjamin Pachevie <b.pachevie@gmail.com>
2023-08-23 11:22:53 +0200

    added nested attributes to granny model

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -2,7 +2,9 @@ class Granny < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_many :categories
+  accepts_nested_attributes_for :categories
   has_many_attached :photos
+
   validates :name, presence: true, uniqueness: true
   validates :presentation, length: { minimum: 14 }
   validates :location, :price, presence: true
