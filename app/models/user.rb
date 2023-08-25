4e308b532c6064762566b7353ebe9eda3d13dcbc
marie intoci <marieintoci@gmail.com>
2023-08-25 15:08:41 +0200

    modification on reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :grannies, dependent: :destroy
   has_many :reservations
+  has_many :booked_grannies, through: :reservations, source: :granny
+
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -10,4 +12,9 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :password, length: { in: 6..20 }
   validates :email, presence: true
+
+  # JIC
+  # def booked_grannies
+  #   self.grannies.map(&:reservations).flatten
+  # end
 end
